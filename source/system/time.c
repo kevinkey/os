66eebd9e0986ba2d0193dcf1ad166dbbfc11210e
Kevin Key <kevin.key@gentex.com>
2026-03-21 12:17:43 -0400

    Remove the static inlines

--- source/system/time.c
+++ source/system/time.c
@@ -1,3 +1,23 @@
 #include "time.h"
 
 uint32_t Time;
+
+time_t time_now(void)
+{
+    return Time;
+}
+
+time_t time_elapsed(time_t start)
+{
+    time_t const now = time_now();
+
+    return (now - start);
+}
+
+void time_increment(time_t amount)
+{
+    time_t now = time_now();
+    time_t total = now + amount;
+
+    Time = total;
+}
--- source/system/time.h
+++ source/system/time.h
@@ -5,26 +5,8 @@
 
 typedef uint32_t time_t;
 
-extern time_t Time;
-
-static inline time_t time_now(void)
-{
-    return Time;
-}
-
-static inline time_t time_elapsed(time_t const start)
-{
-    time_t const now = time_now();
-
-    return (now - start);
-}
-
-static inline void time_increment(time_t const amount)
-{
-    time_t const now = time_now();
-    time_t const total = now + amount;
-
-    Time = total;
-}
+time_t time_now(void);
+time_t time_elapsed(time_t start);
+void time_increment(time_t amount);
 
 #endif
