d91813feed35ad3fbb672c7f76c1020fdea35aa0
Kevin Key <kevin.key@gentex.com>
2026-08-30 09:57:27 -0400

    More renaming of things

--- source/os/os_time.c
+++ source/os/os_time.c
@@ -1,31 +1,31 @@
-#include "time.h"
+#include "os_time.h"
 #include <stdio.h>
 
 static uint32_t Time;
 
-uint32_t time_now(void)
+uint32_t os_time_now(void)
 {
     return Time;
 }
 
-uint32_t time_elapsed(uint32_t start)
+uint32_t os_time_elapsed(uint32_t start)
 {
-    uint32_t const now = time_now();
+    uint32_t const now = os_time_now();
 
     return (now - start);
 }
 
-void time_increment(uint32_t amount)
+void os_time_increment(uint32_t amount)
 {
-    uint32_t now = time_now();
+    uint32_t now = os_time_now();
     uint32_t total = now + amount;
 
     Time = total;
 }
 
-size_t time_string(char time[], size_t length)
+size_t os_time_string(char time[], size_t length)
 {
-    uint32_t t = time_now();
+    uint32_t t = os_time_now();
 
     uint32_t s = t / 1000;
     uint32_t ms = t % 1000;
--- source/os/os_time.h
+++ source/os/os_time.h
@@ -1,11 +1,11 @@
-#ifndef TIME_H
-#define TIME_H
+#ifndef OS_TIME_H
+#define OS_TIME_H
 
 #include "types.h"
 
-uint32_t time_now(void);
-uint32_t time_elapsed(uint32_t start);
-void time_increment(uint32_t amount);
-size_t time_string(char time[], size_t length);
+uint32_t os_time_now(void);
+uint32_t os_time_elapsed(uint32_t start);
+void os_time_increment(uint32_t amount);
+size_t os_time_string(char time[], size_t length);
 
 #endif
